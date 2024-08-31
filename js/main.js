d5e0ec3326d18ac5c082668f1cdf6c9fca0de467
nbc <nbc@snowoods.com>
2024-08-31 23:58:06 +0900

    신제품 소개 비주얼 영역 추가.

--- js/main.js
+++ js/main.js
@@ -37,3 +37,15 @@ window.addEventListener('scroll', _.throttle(function() {
   }
 }, 300)); // 300ms
 // _.throttle(함수, 시간ms)
+
+
+
+// 순차적으로 이미지 나타나게하기
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function (fadeEl, index) {
+  // gsap.to(요소, 지속시간sec, 옵션);
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * .7, // 0.7, 1.4, 2.1, 2.7
+    opacity: 1
+  });
+});
