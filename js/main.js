bed40e6f7e8dd61beb656deb8e6a7a5c17b437c1
nbc <nbc@snowoods.com>
2024-08-31 23:19:40 +0900

    헤더의 우측 배치 추가.

--- js/main.js
+++ js/main.js
@@ -3,14 +3,37 @@ const searchInputEl = searchEl.querySelector('input');
 
 searchEl.addEventListener('click', function () {
   searchInputEl.focus();
-})
+});
 
 searchInputEl.addEventListener('focus', function () {
   searchEl.classList.add('focused');
   searchInputEl.setAttribute('placeholder', '통합검색');
-})
+});
 
 searchInputEl.addEventListener('blur', function () {
   searchEl.classList.remove('focused');
   searchInputEl.setAttribute('placeholder', '');
-})
+});
+
+// 배너 사라지게 하는 기능
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(function() {
+  console.log(window.scrollY);
+  if (window.scrollY > 500) {
+    // 배지 숨기기
+    // gsap.to(요소, 지속시간sec, 옵션);
+    gsap.to(badgeEl, .6, { // 0.6초간 opacity 0으로 변함.
+      opacity: 0,
+      display: 'none'
+    })
+  }
+  else {
+    // 배지 보이기
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    })
+  }
+}, 300)); // 300ms
+// _.throttle(함수, 시간ms)
