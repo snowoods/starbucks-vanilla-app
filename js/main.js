6a777c0fd7afebe888a66fc67046559a6ad7de31
nbc <nbc@snowoods.com>
2024-09-01 13:32:51 +0900

    프로모션 추가.

--- js/main.js
+++ js/main.js
@@ -61,3 +61,40 @@ new Swiper('.notice-line .swiper', {
     disableOnInteraction: false,
   },
 });
+
+new Swiper('.promotion .swiper', {
+  direction: 'horizontal', // 기본값
+  slidesPerView: 3, // 한번에 보여줄 슬라이드 개수
+  spaceBetween: 10, // 슬라이드 사이 여백
+  centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+  loop: true,
+  autoplay: {
+    delay: 5000, // ms
+    disableOnInteraction: false,
+  },  
+  pagination: {
+    el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자
+    clickable: true // 사용자의 페이지 번호 요소 제어 가능 여부
+  },
+  navigation: {
+    prevEl: '.promotion .swiper-prev',
+    nextEl: '.promotion .swiper-next'
+  }
+})
+
+
+// 토글 프로모션
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion')
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener('click', function() {
+  isHidePromotion = !isHidePromotion;
+  if (isHidePromotion) {
+    // 숨김 처리
+    promotionEl.classList.add('hide');
+  }
+  else {
+    // 보임 처리
+    promotionEl.classList.remove('hide');
+  }
+})
